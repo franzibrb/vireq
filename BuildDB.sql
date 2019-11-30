2eefb8aed118f619d0e384517d362f14004700a5
Franzi <franzibrb@googlemail.com>
2019-11-30 04:46:38 +0100

    DB update

--- BuildDB.sql
+++ BuildDB.sql
@@ -11,6 +11,19 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Exportiere Struktur von Tabelle vireq.artikel
+CREATE TABLE IF NOT EXISTS `artikel` (
+  `ArtikelId` int(11) NOT NULL AUTO_INCREMENT,
+  `PaletteId` int(11) NOT NULL DEFAULT 0,
+  `Artikelnummer` varchar(50) COLLATE latin1_bin NOT NULL DEFAULT '0',
+  `Artikelname` varchar(50) COLLATE latin1_bin NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ArtikelId`),
+  KEY `FK_artikel_paletten` (`PaletteId`),
+  CONSTRAINT `FK_artikel_paletten` FOREIGN KEY (`PaletteId`) REFERENCES `paletten` (`PaletteId`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
 -- Exportiere Struktur von Tabelle vireq.lieferanten
 CREATE TABLE IF NOT EXISTS `lieferanten` (
   `LieferantId` int(11) NOT NULL AUTO_INCREMENT,
@@ -23,15 +36,15 @@ CREATE TABLE IF NOT EXISTS `lieferanten` (
   PRIMARY KEY (`LieferantId`),
   KEY `FK_Lieferanten_Users_UserId` (`UserId`),
   CONSTRAINT `FK_Lieferanten_Users_UserId` FOREIGN KEY (`UserId`) REFERENCES `nutzer` (`UserId`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
 
 -- Daten Export vom Benutzer nicht ausgewählt
 
 -- Exportiere Struktur von Tabelle vireq.nutzer
 CREATE TABLE IF NOT EXISTS `nutzer` (
   `UserId` int(11) NOT NULL AUTO_INCREMENT,
-  `UserName` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `Password` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `UserName` varchar(50) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `Password` varchar(50) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
   `LieferantenFile_LieferantenFileName` varchar(50) DEFAULT NULL,
   `LieferantenFile_LieferantenUpdatedAt` datetime DEFAULT NULL,
   `LieferantenFile_LieferantenNummerColumnNameFromCSVImport` varchar(50) DEFAULT NULL,
@@ -44,14 +57,18 @@ CREATE TABLE IF NOT EXISTS `nutzer` (
 
 -- Daten Export vom Benutzer nicht ausgewählt
 
--- Exportiere Struktur von Tabelle vireq.__migrationhistory
-CREATE TABLE IF NOT EXISTS `__migrationhistory` (
-  `MigrationId` varchar(150) NOT NULL,
-  `ContextKey` varchar(300) NOT NULL,
-  `Model` longblob NOT NULL,
-  `ProductVersion` varchar(32) NOT NULL,
-  PRIMARY KEY (`MigrationId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Exportiere Struktur von Tabelle vireq.paletten
+CREATE TABLE IF NOT EXISTS `paletten` (
+  `PaletteId` int(11) NOT NULL AUTO_INCREMENT,
+  `ArtikelFile_ArtikelFileName` varchar(50) COLLATE latin1_bin DEFAULT NULL,
+  `ArtikelFile_PaletteUpdatedAt` datetime DEFAULT NULL,
+  `ArtikelFile_ArtikelnummerColumnNameFromCSVImport` varchar(50) COLLATE latin1_bin DEFAULT NULL,
+  `ArtikelFile_ArtikelnameColumnNameFromCSVImport` varchar(50) COLLATE latin1_bin DEFAULT NULL,
+  `LieferantId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`PaletteId`),
+  KEY `FK_Palette` (`LieferantId`),
+  CONSTRAINT `FK_palette_lieferanten` FOREIGN KEY (`LieferantId`) REFERENCES `lieferanten` (`LieferantId`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
 
 -- Daten Export vom Benutzer nicht ausgewählt
 
